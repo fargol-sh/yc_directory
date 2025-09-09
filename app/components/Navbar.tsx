c17e7d7c1d9363aed093cf4090c16748330923d1
Fargol <fargolsh79@gmail.com>
2025-09-09 14:10:59 +0330

    minor change

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = async () => {
     <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
       <nav className='flex justify-between items-center'>
         <Link href="/">
-          <Image src="/site-icon.svg" alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
@@ -28,9 +28,9 @@ const Navbar = async () => {
                 <button type="submit">Logout</button>
               </form>
               
-              <Link href={`/user/${session?.id}`}>
+              {/* <Link href={`/user/${session?.id}`}>
                 <span>{session?.user?.name}</span>
-              </Link>
+              </Link> */}
             </>
           ): (
             <form action={async () => {
